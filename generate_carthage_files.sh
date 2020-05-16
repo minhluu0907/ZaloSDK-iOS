c8dbeb8f0608b7ca8e13ffaeb979f979861020ee
Luu Nguyen <luunm@vng.com.vn>
2020-05-16 17:28:18 +0700

    Support carthage

---
+++ generate_carthage_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+set -e
+
+
+# version=$(grep "s.version[ ]*="  ZaloSDK.podspec | awk '{print $3}' | tr -d "'")
+version="2.5.0311"
+json_url="https://raw.githubusercontent.com/minhluu0907/ZaloSDK-iOS/master/"
+binary_url="https://github.com/minhluu0907/ZaloSDK-iOS/releases/download/$version"
+carthage_dir="CarthageFiles"
+
+mkdir -p "$carthage_dir"
+
+cd "ZaloSDK/Frameworks"
+
+for f in *; do
+    zip -r -X "$f.zip" "$f"
+    mv "$f.zip" "../../$carthage_dir"
+
+    jsonFile="${f%.*}.json"
+
+    echo "{ \"$version\": \"$binary_url/$f.zip\" }" > "../../$carthage_dir/$jsonFile"
+done
